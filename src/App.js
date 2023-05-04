e4758c6d000f56c9434539697853aa2aefc848d2
rohansah29 <rohansah1997.29@gmail.com>
2023-05-04 23:43:19 +0530

    day_2

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Hero from "./UI/Hero";
+import Header from "./component/Header";
+
+function App() {
+  return <>
+  <Header/>
+  <Hero/>
+  </>;
+}
+
+export default App;
